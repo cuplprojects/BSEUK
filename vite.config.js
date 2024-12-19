97dea823451b553aeab42bf13dcb499594eb6a70
cuplprojects <shivom.chandrakala@gmail.com>
2024-12-19 11:47:50 +0530

    Update to tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['jspdf', 'html2canvas']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/jspdf/, /html2canvas/, /node_modules/]
+    }
+  }
 })
