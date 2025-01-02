ae65ea3fd5df7e7c2248b75a39cdff8733b890b3
cuplprojects <shivom.chandrakala@gmail.com>
2025-01-02 15:52:37 +0530

    Update 7

--- src/pages/Masters/Candidate/bulkCandidates.jsx
+++ src/pages/Masters/Candidate/bulkCandidates.jsx
@@ -248,6 +248,7 @@ const NewFormComponent = () => {
       <div className="flex justify-end mt-4">
         <button
           type="submit"
+          onClick={handleSubmit}
           className={`font-bold py-2 px-6 rounded-md shadow-sm transition duration-150 ease-in-out ${
             theme === "dark"
               ? "bg-purple-600 hover:bg-purple-700 text-white"
